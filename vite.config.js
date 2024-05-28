8143c5c9e591c50728b204b7ac4d4cf855573543
John Ruane <johnmruane@gmail.com>
2024-05-28 09:14:01 +0100

    config

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,6 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  publicDir: 'public',
-  build: {
-    outDir: 'dist',
-  },
   plugins: [react()],
   css: {
     preprocessorOptions: {
